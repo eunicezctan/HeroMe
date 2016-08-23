d80bbe50a2d123fe0c76326de8013bf504b48f41
Eunice Tan <eunice_tan@byu.edu>
2016-08-23 01:24:25 -0600

    initial commit

---
+++ app/src/main/java/com/example/android/herome/Activities/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.herome.Activities;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.ActivityChooserView;
+
+import com.example.android.herome.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
