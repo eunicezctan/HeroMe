806454470c9416e9cc0a9e79d721875dd7345e79
Eunice Tan <eunice_tan@byu.edu>
2016-08-23 15:32:49 -0600

    interface appear in MainActivity

--- app/src/main/java/com/example/android/herome/Activities/MainActivity.java
+++ app/src/main/java/com/example/android/herome/Activities/MainActivity.java
@@ -1,17 +1,35 @@
 package com.example.android.herome.Activities;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.widget.ActivityChooserView;
-
+import android.support.v7.app.AppCompatActivity;
 import com.example.android.herome.R;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
+import com.example.android.herome.Fragments.MainFragment;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends AppCompatActivity implements MainFragment.MainFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null){
+            fragment = new MainFragment();
+            manager.beginTransaction().add(R.id.fragment_container,fragment).commit();
+
+        }
+
+    }
+
+
+    @Override
+    public void onMainFragmentInteraction(Uri uri) {
+
     }
 }
